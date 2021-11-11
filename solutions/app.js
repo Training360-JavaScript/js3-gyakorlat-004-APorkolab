9b1dc90434e486b3b692b14fe8bbc5cf5d6c32e5
Porkoláb Ádám <adam.porkolab@gmail.com>
2021-11-11 11:27:19 +0100

    Final.

--- solutions/app.js
+++ solutions/app.js
@@ -3,12 +3,12 @@ import checkVisa from './visa';
 import checkIP from './ip';
 import checkMac from './mac';
 
-const checker = (macAddress = 0) => {
+const checker = {
 	rules: {
-		checkVisa(),
-		checkIP(),
-		checkMac()
-	};
+		checkVisa,
+		checkIP,
+		checkMac,
+	},
 	validate(string, checkType) {
 		if (checkType === 'ip') {
 			return checkIP(string);
